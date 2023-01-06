a77312bee49abef544950fd573e7aff5c83715af
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2023-01-06 15:39:08 -0300

    avancei aulas no curso em video

---
+++ cursoEmVideoPython3/ex001.py
@@ -0,0 +1,3 @@
+n1 = input('Digite um Número: ')
+n2 = input('Digite outro Número: ')
+res = input()
---
+++ cursoEmVideoPython3/pythonTeste/aula07a.py
@@ -0,0 +1,5 @@
+n = float(input('Primeira nota do aluno:'))
+n1= float(input('Segunda nota do aluno:'))
+n2= (n+n1)/2
+print(f'A média entre {n} e {n1} é igual a {n2:.1f} .')
+
