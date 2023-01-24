693964391c51a05092d94f26df222f8b8694bd51
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2023-01-24 16:48:13 -0300

    finalizei ex045

--- exercicios/ex045.py
+++ exercicios/ex045.py
@@ -1,7 +1,12 @@
 from random import randint
 from time import sleep
-jogada = int(input('Suas opções:\n[ 0 ] PEDRA\n[ 1 ] PAPEL\n[ 2 ] TESOURA\nQual a sua jogada? '))
-computador = 
+
+itens = ('Pedra','Papel','Tesoura')
+pc = randint(0,2)
+
+print(f'{" JO KEN PO!!! ":=^40}')
+print('Suas opções:\n[ 0 ] PEDRA\n[ 1 ] PAPEL\n[ 2 ] TESOURA')
+jogador = int(input('Qual a sua jogada? '))
 
 print('JO')
 sleep(1)
@@ -11,14 +16,21 @@ print('PO!!!')
 sleep(1)
 
 print('-='*10)
-print(f'Computador jogou {computador}')
-if jogada == '0':
-    print('Jogador jogou Pedra')
-elif jogada == 1:
-    print('Jogador jogou Papel')
-elif jogada == 2:
-    print('Jogador jogou Tesoura')
+print(f'Computador jogou {itens[pc]}')
+print(f'Jogador jogou {itens[jogador]}')
 print('-='*10)
+
+if jogador == pc:
+    print('DEU EMPATE! JOGUE OUTRA VEZ')
+
+elif jogador == 0 and pc == 1 or jogador == 1 and pc == 2 or jogador == 2 and pc == 0:
+    print('JOGADOR PERDE')
+elif jogador == 1 and pc == 0 or jogador == 2 and pc == 1 or jogador == 2 and pc == 0:
+    print('JOGADOR GANHA')
+else:
+    print('OPÇÃO INVÁLIDA! DIGITE NOVAMENTE')
+
+
 
 
  
