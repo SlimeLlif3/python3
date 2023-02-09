af9c973252e04aa5a95617e9791401c2d653a2dd
SlimeLlif3 <110485993+SlimeLlif3@users.noreply.github.com>
2023-02-09 14:13:36 -0300

    Update ex058.py

--- exercicios/ex058.py
+++ exercicios/ex058.py
@@ -1,2 +1,28 @@
+from random import randint
 print('Sou seu computador...\nAcabei de pensar em um número entre 0 e 10.\nSerá que você consegue adivinhar qual foi ?')
-palpite = int(input('Qual é seu palpite? '))
+jogador = int(input('Qual é seu palpite? '))
+pc = randint(0,10)
+print(pc)
+while jogador != pc:    
+    if jogador > pc:
+        menor = int(input('K K K, Informe um valor MENOR seu otário: '))
+    elif menor < pc:
+        maior = int(input('K K K, Informe um valor MAIOR seu otário: '))
+
+
+        
+
+
+        
+        
+            
+
+   
+
+
+        
+        
+
+        
+
+
